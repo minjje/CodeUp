0a5b344c72d2b412bb5edfdb56c22fe4b5a8e446
minjje <94779437+minjje@users.noreply.github.com>
2022-08-21 11:47:22 +0900

    Create 입출력.cpp

---
+++ 입출력.cpp
@@ -0,0 +1,47 @@
+// 1010 정수 1개를 입력받아 그대로 출력하기
+/*
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", n);
+    return 0;
+}
+*/
+
+// 1011 문자 1개를 입력받아 그래도 출력하기
+/*
+#include <stdio.h>
+int main(void)
+{
+    char x;
+    scanf("%c", &x);
+    printf("%c", x);
+    return 0;
+}
+*/
+
+// 1012 실수 1개를 입력받아 그대로 출력하기
+/*
+#include <stdio.h>
+int main(void)
+{
+    float x;
+    scanf("%f", &x);
+    printf("%f", x);
+    return 0;
+}
+*/
+
+// 1013 정수 2개를 입력받아 그대로 출력하기
+/*
+#include <stdio.h>
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d %d", a, b);
+    return 0;
+}
+*/
