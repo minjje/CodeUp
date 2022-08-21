98c8a44a98c35420dfc5d2949d8842598ffc09dd
minjje <94779437+minjje@users.noreply.github.com>
2022-08-21 14:27:40 +0900

    Create 데이터형.cpp

---
+++ 데이터형.cpp
@@ -0,0 +1,36 @@
+// 1028 정수 1개를 입력받아 그대로 출력하기2
+/*
+#include <stdio.h>
+int main(void)
+{
+   unsigned int a;
+   scanf("%u", &a);
+   printf("%u", a);
+   return 0;
+}
+*/
+
+// 1029 실수 1개를 입력받아 그대로 출력하기2
+/*
+#include <stdio.h>
+int main(void)
+{
+    double d;
+    scanf("%lf", &d);
+    printf("%.11lf", d);
+    return 0;
+}
+*/
+
+// 1030 정수 1개를 입력받아 그대로 출력하기3
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    long long int n;
+    scanf("%lld", &n);
+    printf("%lld", n);
+    return 0;
+}
+*/
