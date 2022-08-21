147c8e7e50896222259e806f1c51d6bb142be415
minjje <94779437+minjje@users.noreply.github.com>
2022-08-21 14:46:18 +0900

    Update and rename 출력변환.cpp to 4.출력변환.cpp

---
+++ 4.출력변환.cpp
@@ -0,0 +1,83 @@
+// 1031 10진 정수 1개 입력받아 8진수로 출력하기
+/*
+#include <stdio.h>
+int main(void)
+{
+    int a;
+    scanf("%d", &a);
+    printf("%o", a); //%o: 8진수로 출력
+    return 0;
+}
+*/
+
+// 1032 10진수 정수 1개 입력받아 16진수로 출력하기1
+/*
+#include <stdio.h>
+int main(void)
+{
+    int a;
+    scanf("%d", &a);
+    printf("%x", a); //%x: 16진수로 소문자로 출력
+    return 0;
+}
+*/
+
+// 1033 10진수 정수 1개를 입력받아 16진수로 출력하기2
+/*
+#include <stdio.h>
+int main(void)
+{
+    int a;
+    scanf("%d", &a);
+    printf("%X", a); //%X: 16진수 대문자로 출력
+    return 0;
+}
+*/
+
+// 1034 8진수 정수 1개를 입력받아 10진수로 출력하기
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    scanf("%o", &a);
+    printf("%d", a);
+    return 0;
+}
+*/
+
+// 1035 16진수 정수 1개를 입력받아 8진수로 출력하기
+/*
+#include <stdio.h>
+int main(void)
+{
+    int a;
+    scanf("%x", &a);
+    printf("%o", a);
+    return 0;
+}
+*/
+
+// 1036 영문자 1개 입력받아 10진수로 출력하기
+/*
+#include <stdio.h>
+int main(void)
+{
+    char t;
+    scanf("%c", &t);
+    printf("%d", t);
+    return 0;
+}
+*/
+
+// 1037 정수를 입력받아 아스키 문자로 출력하기
+
+#include <stdio.h>
+int main(void)
+{
+    int a;
+    scanf("%d", &a);
+    printf("%c", a);
+    return 0;
+}
