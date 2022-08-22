973270c0096c7eb4b3620a9460d3905cf3e76e73
minjje <94779437+minjje@users.noreply.github.com>
2022-08-22 20:59:19 +0900

    Create 9.비트단위논리연산.cpp

---
+++ 9.비트단위논리연산.cpp
@@ -0,0 +1,46 @@
+// 1059 비트단위로 NOT하여 출력하기
+/*
+#include <stdio.h>
+int main(void)
+{
+    int a;
+    scanf("%d", &a);
+    printf("%d", ~a); // NOT
+    return 0;
+}
+*/
+
+// 1060 비트단위로 AND하여 출력하기
+/*
+#include <stdio.h>
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a&b); // AND
+    return 0;
+}
+*/
+
+// 1061 비트단위로 OR하여 출력하기
+/*
+#include <stdio.h>
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a|b); // OR
+    return 0;
+}
+*/
+
+// 1062 비트단위로 XOR하여 출력하기
+
+#include <stdio.h>
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a^b); // XOR
+    return 0;
+}
