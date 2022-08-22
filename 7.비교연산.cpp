3d95942745c0d8b894054db5c1c8680dd6f784e7
minjje <94779437+minjje@users.noreply.github.com>
2022-08-22 20:34:03 +0900

    Create 7.비교연산.cpp

---
+++ 7.비교연산.cpp
@@ -0,0 +1,50 @@
+// 1049 두 정수를 입력받아 비교하기1
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a>b);
+    return 0;
+}
+*/
+
+// 1050 두 정수를 입력받아 비교하기2
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a==b);
+    return 0;
+}
+*/
+
+// 1051 두 정수를 입력받아 비교하기3
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", b>=a);
+    return 0;
+}
+*/
+
+// 1052 두 정수를 입력받아 비교하기4
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a!=b);
+    return 0;
+}
